bcfe1db929fe6972a8e041eaf920fff7760a8b67
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2016-03-22 11:56:39 +0000

    Created a todo app with OpenFin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newItem: 'fish',
+      todos: ['test', 'test2']
+    };
+  }
+
+  addItem() {
+    this.setState({
+      newItem: '',
+      todos: [...this.state.todos, this.state.newItem]
+    });
+  }
+
+  updateNewItem(event) {
+    this.setState({
+      newItem: event.target.value
+    });
+  }
+
+  deleteItem(todo) {
+    const index = this.state.todos.indexOf(todo);
+    const newTodos = this.state.todos.slice();
+    newTodos.splice(index, 1);
+    this.setState({
+      todos: newTodos
+    });
+  }
+
   render() {
+    const todoList = this.state.todos.map(d => (
+        <li>{d} <button onClick={this.deleteItem.bind(this, d)}>x</button></li>
+      )
+    );
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Todo List FTW!</h1>
+        <input value={this.state.newItem} onChange={this.updateNewItem.bind(this)}></input>
+        <button onClick={this.addItem.bind(this)}>add</button>
+        <ul>{todoList}</ul>
+      </div>
     );
   }
 }
